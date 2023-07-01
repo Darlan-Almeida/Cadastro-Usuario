97c56ea6d3aaf321d928e4c3281ee9a99d084de9
Darlan almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-07-01 14:37:11 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,90 +1,59 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 class Pessoa {
-  private String nome;
-  private String cpf;
-  private int anoDeNascimento;
-  private double altura;
-      
+    private String nome;
+    private String cpf;
+    private int anoDeNascimento;
+    private double altura;
 
+    public Pessoa(String nome, String cpf, int anoDeNascimento, double altura) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.anoDeNascimento = anoDeNascimento;
+        this.altura = altura;
+    }
 
-  public Pessoa( String nome , String cpf , int anoDeNascimento , double altura  ){
-    
-    this.nome = nome;
-    
-    this.cpf = cpf;
-    
-    this.anoDeNascimento = anoDeNascimento;
-    
-    this.altura = altura;
-    
-    
-  }
-  
-  public String getNome(){
-    return nome;
-    
-  }
+    public String getNome() {
+        return nome;
+    }
 
-  public void setNome( String nome){
-    this.nome = nome;
-    
-  }
-  
-  public String getCpf(){
-    return cpf;
-    
-  }
-  
-  public void setCpf( String cpf){
-    this.cpf = cpf;
-    
-  }
-  
-  public int getAnoDeNascimento(){
-    return anoDeNascimento;
-    
-  } 
-  
-  public void setAnoDeNascimento( int anoDeNascimento ){
-    this.anoDeNascimento = anoDeNascimento;
-    
-  }  
-  
-  
-  public double getAltura(){
-    return altura;
-    
-  }  
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-  public void setAltura( double altura){
-    this.altura = altura;
-    
-  }
-  
-  public int setidade( int ano){
-    return ano - anoDeNascimento;
-    
-    
-    //não consegui formatar a data:
-    
-    /*Date hoje = new Date(); //pegar a data de hoje- Date
-    
-    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-    formato.format(hoje);// formatar a data de hoje 
-    
-    
-    
-    // inputar a data de dataDeNascimento
-    
-    Date nascimento = new Date( dataDeNascimento ); //pegar a data de hoje- Date
-    SimpleDateFormat formato2 = new SimpleDateFormat("dd/MM/yyyy");
-    formato2.format(nascimento);
-    
-    // comparar as datas para obter a idade
-    
-      int idade = nascimento.compareTo(hoje);
-    
-    return idade;*/
-    
-  }
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getAnoDeNascimento() {
+        return anoDeNascimento;
+    }
+
+    public void setAnoDeNascimento(int anoDeNascimento) {
+        this.anoDeNascimento = anoDeNascimento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getIdade() {
+        LocalDate hoje = LocalDate.now();
+        LocalDate dataNascimento = LocalDate.of(anoDeNascimento, 1, 1); // Supondo que o dia e mês de nascimento sejam 1 de janeiro
+        Period periodo = Period.between(dataNascimento, hoje);
+        int anos = periodo.getYears();
+        int meses = periodo.getMonths();
+        int dias = periodo.getDays();
+
+        return anos + " anos, " + meses + " meses e " + dias + " dias";
+    }
 }
-    
